616e396933bbec017227e7c19f36c0bfb8d6b25e
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-31 21:15:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,13 @@
 def my_collect(array)
-  
+  modified_array = []
+  i = 0
+  if block_given?
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+  else
+    puts "Give me an array!"
+  end
+  modified_array
 end
