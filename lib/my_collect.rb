5212372c002a6a1099e817aefed577ba8e5077cf
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-31 21:29:02 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,11 +3,9 @@ def my_collect(array)
   i = 0
   if block_given?
     while i < array.length
-      yield array[i]
-      modified_array << array[i]
+      modified_array << yield array[i]
+      i++
     end
-    i++
-    
   else
     puts "Give me an array!"
   end
